24a00ad447fa8011876e31410f1e81fb6b133275
sakash5940 <akashpatil5940@gmail.com>
2024-10-21 19:17:35 +0530

    Add files via upload

---
+++ string2task.js
@@ -0,0 +1,27 @@
+//task1
+let str = "Hello World!";
+let replace=str.replace("World!","My Dear Friend")
+console.log(replace);
+
+//task2
+let a="Hungry...!";
+let repeat=a.repeat(10)
+console.log(repeat);
+
+//task3
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+let lastindex= sentence.lastIndexOf("fox")
+console.log(lastindex);
+
+//task4
+let message = "Hello, world!";
+console.log( message.startsWith("Hello"));
+
+//task5
+let fileName = "document.pdf";
+console.log(fileName.endsWith("pdf"));
+
+   
+
+
+
